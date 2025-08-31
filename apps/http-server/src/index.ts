f54802ec3b204ecf55b1d2bea3eddba18c40dc66
yug <yugdesai517@gmail.com>
2025-08-31 22:48:05 +0530

    server created - http - ws

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import {client} from "@repo/db/client"
+const app = express()
+app.use(express.json())
+
+app.listen(3000)
+app.get('/',(req,res) => {
+    res.send("Hello from yug!")
+})
+
+app.post("/signup" ,async (req,res) => {
+    const username = req.body.username
+    const password = req.body.password
+    const user = await client.user.create({
+        data:{
+            username,
+            password
+        }
+    })
+    res.json({
+        message : "Signup successfully",
+        id : user.id
+    })
+})
+app.listen(3000)
