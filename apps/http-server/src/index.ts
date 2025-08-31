67adb694db529477d2e65241742be4c301bfc723
yug <yugdesai517@gmail.com>
2025-09-01 00:15:31 +0530

    web + http + ws working

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -3,12 +3,14 @@ import {client} from "@repo/db/client"
 const app = express()
 app.use(express.json())
 
-app.listen(3000)
+// app.listen(3000)
 app.get('/',(req,res) => {
     res.send("Hello from yug!")
 })
 
 app.post("/signup" ,async (req,res) => {
+    console.log("in signup");
+    
     const username = req.body.username
     const password = req.body.password
     const user = await client.user.create({
@@ -22,4 +24,4 @@ app.post("/signup" ,async (req,res) => {
         id : user.id
     })
 })
-app.listen(3000)
+app.listen(3002)
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -2,17 +2,17 @@ import { WebSocketServer } from "ws";
 import {client} from "@repo/db/client"
 const server = new WebSocketServer({ port: 3001 });
 
-server.on("connection", (socket) => {
+server.on("connection",async (socket) => {
+    const res = await client.user.findFirst()
+//    const res =  await client.user.create({
+//         data : {
+//             username : Math.random().toString(),
+//             password : Math.random().toString()
+//         }
+//     })
+    console.log(res);
+    socket.send(`Hi there you are connected to server ${res?.password}`);
 
-    client.user.create({
-        data : {
-            username : Math.random().toString(),
-            password : Math.random().toString()
-        }
-    })
-  console.log("Hi there you are connected to server");
-
-  
 });
 
 console.log("WebSocket server is running on ws://localhost:3001");
